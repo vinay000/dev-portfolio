5f9eb2e242c2c2097c92622b41b802be4d2fb219
vinay000 <vky11504119@gmail.com>
2025-07-14 23:44:39 +0530

    Added prod basic

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/dev-portfolio",
+  base: "/dev-portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
